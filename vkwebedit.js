ce1065696985cd87231c7d59141c6ea629f0b10c
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2016-01-12 19:21:12 +0100

    Some copy-pasted code just to test the WebMIDI API is working

--- vkwebedit.js
+++ vkwebedit.js
@@ -55,7 +55,7 @@ function buildAppArea() {
       },
       {
          "code":51,
-         "label":"EG Sustan"
+         "label":"EG Sustain"
       },
       {
          "code":52,
@@ -79,5 +79,48 @@ function buildAppArea() {
     "id": "controls",
     html: items.join("")
   }).appendTo("#apparea");
+
+  if (navigator.requestMIDIAccess) {
+    // Try to connect to the MIDI interface.
+    navigator.requestMIDIAccess().then(onSuccess, onFailure);
+  } else {
+    console.log("Web MIDI API not supported!");
+  }
+}
+
+
+// Function executed on successful connection
+function onSuccess(interface) {
+
+  var noteon,
+      noteoff,
+      outputs = [];
+
+  console.log(interface);
+
+  // Grab an array of all available devices
+  var iter = interface.outputs.values();
+  for (var i = iter.next(); i && !i.done; i = iter.next()) {
+    outputs.push(i.value);
+  }
+
+  // Craft 'note on' and 'note off' messages (channel 3, note number 60 [C3], max velocity)
+  noteon = [0x92, 60, 127];
+  noteoff = [0x82, 60, 127];
+
+  // Send the 'note on' and schedule the 'note off' for 1 second later
+  outputs[0].send(noteon);
+  setTimeout(
+    function() {
+      outputs[0].send(noteoff);
+    },
+    1000
+  );
+
+}
+
+// Function executed on failed connection
+function onFailure(error) {
+  console.log("Could not connect to the MIDI interface");
 }
 
